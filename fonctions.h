2cf00cd8dcc63d04d63643f6a2807b6f5a85c326
ThomasSoulliaert <tsoullia@gmail.com>
2024-03-29 11:28:21 +0100

    Test

---
+++ fonctions.h
@@ -0,0 +1,8 @@
+//
+// Created by tsoul on 29/03/2024.
+//
+
+#ifndef PROJET_C_FONCTIONS_H
+#define PROJET_C_FONCTIONS_H
+
+#endif //PROJET_C_FONCTIONS_H
--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 //
 // Created by tsoul on 29/03/2024.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
-TEst
+#define REALOC_SIZE 256
+
+int main()
+{
+    typedef struct
+    {
+        char Titre[100];
+        int Taille_physique;
+        int Taille_logique;
+        int Data[REALOC_SIZE];
+    }column;
+
+    printf("Hello world\n");
+    printf("Test OK");
+    return 0;
+}
