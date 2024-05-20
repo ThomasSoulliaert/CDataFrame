cdf5e125a0acde7a14c637f290b7171f73a49032
Thomas <150612288+ThomasSoulliaert@users.noreply.github.com>
2024-05-20 18:23:01 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -18,44 +18,46 @@ typedef struct
     int TP;
     int TL;
     int * Data;
-}COLUMN; /// Nouveau type de structure : type "COLUMN"
+}COLUMN; // Nouveau type de structure : type "COLUMN"
 
-/// Partie : Fonctions de base
-COLUMN *create_column(char *titre);
-int insert_value(COLUMN *colonne, int valeur);
-void delete_column(COLUMN **colonne);
-void print_col(COLUMN *colonne);
-int occurrence(COLUMN *colonne,int x);
-int position(COLUMN *colonne, int x);
-int superieur(COLUMN *colonne, int x);
-int inferieur(COLUMN *colonne, int x);
-int egale(COLUMN *colonne, int x);
+// Partie I : Colonne
+COLUMN *create_column(char*);
+int insert_value(COLUMN*, int);
+void delete_column(COLUMN**);
+void print_col(COLUMN*);
+int occurrence(COLUMN*, int);
+int position(COLUMN*, int);
+int superieur(COLUMN*, int);
+int inferieur(COLUMN*, int);
+int egale(COLUMN*, int);
 
-/// Partie : Alimentation
-COLUMN** creat_DATAFRAME(int taille);
-void fill_DataFrame(COLUMN** DataFrame, int nombreColonne);
 
-/// Partie : Affichage
-void print_DataFrame(COLUMN** DataFrame, int nombreColonne);
-void print_DataFrameLimitLine(COLUMN** DataFrame, int nombreColonne, int LigneLimite);
-void print_DataFrameLimitColumn(COLUMN** DataFrame, int nombreColonne, int ColonneLimite);
+// Partie II : DataFrame
+// Partie 2.1 : Alimentation
+COLUMN** creat_DATAFRAME(int);
+void fill_DataFrame(COLUMN**, int);
 
-/// Partie : Opérations
-void fill_DataFrameLine(COLUMN** DataFrame, int nombreColonne);
-void del_DataFrameLine(COLUMN** DataFrame, int nombreColonne);
-void add_DataFrameColumn(COLUMN** DataFrame, int *nombreColonne);
-void del_DataFrameColumn(COLUMN** DataFrame, int *nombreColonne);
-void rename_Column(COLUMN** DataFrame, int *nombreColonne);
-int Research_Value(COLUMN** DataFrame, int nombreColonne, int x);
-void ReplaceValue(COLUMN** DataFrame, int nombreColonne);
-void printName(COLUMN** DataFrame, int nombreColonne);
+// Partie 2.2 : Affichage
+void print_DataFrame(COLUMN** , int);
+void print_DataFrameLimitLine(COLUMN**, int, int);
+void print_DataFrameLimitColumn(COLUMN**, int, int);
 
-/// Partie : Analyses & Statistiques
-int numberLines_DataFrame(COLUMN** DataFrame, int nombreColonne);
-int numberColumns_DataFrame(COLUMN** DataFrame);
-int numberValues_Equal_DataFrame(COLUMN** DataFrame, int x, int nombreColonne);
-int numberValues_Sup_DataFrame(COLUMN** DataFrame, int x, int nombreColonne);
-int numberValues_Inf_DataFrame(COLUMN** DataFrame, int x, int nombreColonne);
+// Partie 2.3 : Opérations usuelles
+void fill_DataFrameLine(COLUMN** , int);
+void del_DataFrameLine(COLUMN** , int);
+void add_DataFrameColumn(COLUMN** , int*);
+void del_DataFrameColumn(COLUMN** , int*);
+void rename_Column(COLUMN** , int*);
+int Research_Value(COLUMN** , int , int);
+void ReplaceValue(COLUMN** , int);
+void printName(COLUMN** , int);
+
+// Partie 2.4 : Analyses & Statistiques
+int numberLines_DataFrame(COLUMN**, int);
+int numberColumns_DataFrame(COLUMN** , int);
+int numberValues_Equal_DataFrame(COLUMN** , int , int);
+int numberValues_Sup_DataFrame(COLUMN** , int , int);
+int numberValues_Inf_DataFrame(COLUMN** , int , int);
 
 
 #endif //PROJET_C_FONCTIONS_H
